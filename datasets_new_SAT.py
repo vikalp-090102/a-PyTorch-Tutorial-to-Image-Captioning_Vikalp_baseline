51d8a29b9735be893657d632ca0ee06efa2686bb
Vikalp Srivastava <114852983+vikalp-090102@users.noreply.github.com>
2025-06-09 21:20:16 +0300

    datasets_new_SAT.py

---
+++ datasets_new_SAT.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from PIL import Image
+import torch
+from torch.utils.data import Dataset
+import os
+
+class IndianaXrayDataset(Dataset):
+    def __init__(self, image_dir, projections_csv, reports_csv, split, transform=None):
+        self.image_dir = image_dir
+        self.transform = transform
+        self.split = split.upper()
+        
+        # Load data
+        self.projections = pd.read_csv(projections_csv)
+        self.reports = pd.read_csv(reports_csv)
+
+        # Merge data on UID
+        self.data = self.projections.merge(self.reports, on='uid')
+        
+    def __getitem__(self, idx):
+        row = self.data.iloc[idx]
+        img_path = os.path.join(self.image_dir, row['image_path'])  # Adjust as needed
+        image = Image.open(img_path).convert("RGB")
+        
+        if self.transform:
+            image = self.transform(image)
+        
+        caption = row['findings']  # Extracting caption from reports
+        return image, caption
+
+    def __len__(self):
+        return len(self.data)
